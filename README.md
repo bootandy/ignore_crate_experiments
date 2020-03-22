f88eafad3c0c8619b0f6afd769fb78678192fe18
andy.boot <bootandy@gmail.com>
2020-03-22 12:52:56 +0000

    hack

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ignore_crate_experiments
 Experimenting with Burntsushi's ignore crate for dir traversal.
 
-How to go from a WalkDir entry into having everything stored as a hashmap as quickly as possible
+How to go from a WalkDir entry into having everything stored as a hashmap as quickly as possible. 
 
-The channel method seems slightly quicker than the shared hashmap method on my box.
+All results are based on my own personal machine.
+
+The channel method seems slightly quicker than the shared hashmap method.
+
+Then I experimented with doing more work in the receiver and transmitter threads.
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::env;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-
+mod channel_vs_hashmap;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,20 +21,33 @@ fn main() {
             &args[1]
         }
     };
-    //walk_para_hash_map(dir);
-    walk_para_channel(dir);
+    println!("Analizing {}", dir);
+
+    // Using a channel was ~ 10% faster than using a shared hashmap
+    //channel_vs_hashmap::walk_para_hash_map(dir);
+    //channel_vs_hashmap::walk_para_channel(dir);
+
+    // _in_rec 'doing the work in the single receiver thread' was aprox twice as fast
+    walk_para_channel_in_rec(dir);
+    //walk_para_channel_in_tx(dir);
+
+    // Introducing 2 reader threads killed performance, probably all the lock spinning
+    //walk_para_channel_in_rec_para(dir);
+
+    // Tried 2 reader threads each building their own hashmap but it looks like most of the work 
+    // is actually being done in the write threads.
 }
 
-fn walk_para_channel(dir: &str) {
-    println!("Analizying {}", dir);
-    let (tx, rx) = channel::bounded::<PathBuf>(1000);
+fn walk_para_channel_in_tx(dir: &str) {
+    let (tx, rx) = channel::bounded::<(PathBuf, u64)>(1000);
 
     // Receiver thread
     let stdout_thread = thread::spawn(move || {
         let mut hash: HashMap<String, u64> = HashMap::new();
         for dent in rx {
-            //println!("{:?}", dent);
-            hash.insert(dent.to_str().unwrap().to_string(), 1);
+            let (path, size) = dent;
+            let s = hash.entry(path.to_str().unwrap().to_string()).or_insert(0);
+            *s += size;
         }
         hash
     });
@@ -46,7 +59,10 @@ fn walk_para_channel(dir: &str) {
         Box::new(move |path| {
             match path {
                 Ok(p) => {
-                    txc.send(p.into_path()).unwrap();
+                    let u = p.metadata().unwrap().len();
+                    for pa in p.into_path().ancestors() {
+                        txc.send((pa.to_path_buf(), u) ).unwrap();
+                    }
                 },
                 Err(_) => {}
             };
@@ -57,25 +73,105 @@ fn walk_para_channel(dir: &str) {
     drop(tx);
     let hash = stdout_thread.join().unwrap();
     println!("Found {:?} files ", hash.len());
+    //println!("{:?}", hash);
 }
 
-fn walk_para_hash_map(dir: &str) {
-    println!("Analizying {}", dir);
+fn walk_para_channel_in_rec(dir: &str) {
+    let (tx, rx) = channel::bounded::<(PathBuf, u64)>(1000);
+
+    // Receiver thread
+    let stdout_thread = thread::spawn(move || {
+        let mut hash: HashMap<String, u64> = HashMap::new();
+        for dent in rx {
+            let (path, size) = dent;
+            for p in path.ancestors() {
+                let s = hash.entry(p.to_str().unwrap().to_string()).or_insert(0);
+                *s += size;
+            }
+        }
+        hash
+    });
+
+    // Walk the dir tree thread
     let builder = WalkBuilder::new(dir);
-    let hash: HashMap<PathBuf, u64> = HashMap::new();
-    let mutex_hash = Arc::new(Mutex::new(hash));
     builder.build_parallel().run(|| {
-        let the_hash = mutex_hash.clone();
+        let txc = tx.clone();
         Box::new(move |path| {
             match path {
                 Ok(p) => {
-                    let mut h = the_hash.lock().unwrap();
-                    h.insert(p.into_path(), 1);
+                    let u = p.metadata().unwrap().len();
+                    txc.send((p.into_path(), u) ).unwrap();
                 },
                 Err(_) => {}
             };
             WalkState::Continue
         })
     });
-    println!("Found {:?} files ", mutex_hash.lock().unwrap().len());
+
+    drop(tx);
+    let hash = stdout_thread.join().unwrap();
+    println!("Found {:?} files ", hash.len());
+    //println!("{:?}", hash);
+}
+
+fn walk_para_channel_in_rec_para(dir: &str) {
+    let (tx, rx) = channel::bounded::<(PathBuf, u64)>(1000);
+    let rx2 = rx.clone();
+
+    let hash: HashMap<String, u64> = HashMap::new();
+    let mutex_hash = Arc::new(Mutex::new(hash));
+    // let mutex_hash2 = mutex_hash.clone();
+    let mh = mutex_hash.clone();
+
+    let hash: HashMap<String, u64> = HashMap::new();
+    let mutex_hash2 = Arc::new(Mutex::new(hash));
+
+    // Receiver thread
+    let stdout_thread = thread::spawn(move || {
+        let local_hash = mutex_hash.clone();
+        for dent in rx {
+            let (path, size) = dent;
+            let mut h = local_hash.lock().unwrap();
+            for p in path.ancestors() {
+                let s = h.entry(p.to_str().unwrap().to_string()).or_insert(0);
+                *s += size;
+            }
+        }
+    });
+
+    // gratuotus copy paste coding
+    let stdout_thread2 = thread::spawn(move || {
+        let local_hash = mutex_hash2.clone();
+        for dent in rx2 {
+            let (path, size) = dent;
+            let mut h = local_hash.lock().unwrap();
+            for p in path.ancestors() {
+                let s = h.entry(p.to_str().unwrap().to_string()).or_insert(0);
+                *s += size;
+            }
+        }
+    });
+
+    // Walk the dir tree thread
+    let builder = WalkBuilder::new(dir);
+    builder.build_parallel().run(|| {
+        let txc = tx.clone();
+        Box::new(move |path| {
+            match path {
+                Ok(p) => {
+                    let u = p.metadata().unwrap().len();
+                    txc.send((p.into_path(), u)).unwrap();
+                },
+                Err(_) => {}
+            };
+            WalkState::Continue
+        })
+    });
+
+    drop(tx);
+
+    stdout_thread.join().unwrap();
+    stdout_thread2.join().unwrap();
+    println!("Found {:?} files ", mh.lock().unwrap().len());
+    //println!("{:?}", hash);
 }
